b7ac945a5c0cbdce4be45305a4c3413af544c619
PapaSmurfinator <m.amirhan159@gmail.com>
2024-08-08 18:52:51 +0300

    add domain models: Volunteer; SocialNetwork

---
+++ backend/src/PetFamily.Domain/Models/SocialNetwork.cs
@@ -0,0 +1,7 @@
+﻿namespace PetFamily.Domain.Models;
+
+public sealed class SocialNetwork
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
---
+++ backend/src/PetFamily.Domain/Models/Volunteer.cs
@@ -0,0 +1,13 @@
+﻿namespace PetFamily.Domain.Models;
+
+public sealed class Volunteer
+{
+    public Guid Id { get; private set; }
+    public string FIO { get; private set; } = string.Empty!;
+    public string Description { get; private set; } = string.Empty;
+    public byte YearsOfExperience { get; set; } = 0;
+    public uint NumberOfPets { get; set; } = 0;
+    public string PhoneNumber{ get; private set; } = string.Empty;
+    public List<SocialNetwork> SocialNetworks { get; set; } = new();
+    public Requisites Requisites { get; private set; } = new();
+}
